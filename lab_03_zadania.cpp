977d41b7ff1d887919b88245830e9ac24d1fb6bf
65544 <92386805+65544@users.noreply.github.com>
2021-11-03 17:23:01 +0100

    Add files via upload

---
+++ lab_03_zadania.cpp
@@ -0,0 +1,125 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+
+
+	//-----------Zadanie 1-------------------
+		
+	//	int n, suma = 0, srednia, i, a;
+	//	cout << "Podaj liczbe studentow: ", cin >> n;
+	//	i = 0;
+	//	while (i < n)
+	//	{
+	//		cout << "Podaj liczbe punktow studenta: ", cin >> a;
+	//		i++;
+	//		suma += a;
+
+	//	}
+	//	cout << suma / n;
+	
+	//-----------Zadanie 2-------------------
+	// 
+	//	int n,i, a;
+	//	float	suma = 0;
+	//	cout << "Podaj liczbe studentow: ", cin >> n;
+	//	i = 0;
+	//		while (i < n)
+	//		{
+	//			cout << "Podaj liczbe punktow studenta: ", cin >> a;
+	//			if (a >= 0 && a <= 100) {
+	//				i++;
+	//				suma += a;
+	//			}
+	//			else
+	//			{
+	//				cout << "Niepoprawna liczba! \n";
+	//			}
+	//	}
+	//		cout << suma / n;
+	
+	//-----------Zadanie 3 a)-------------------
+	
+		//int n, i, a;
+		//float	suma = 0;
+		//cout << "Podaj liczbe studentow: ", cin >> n;
+		//i = 0;
+		//while (i < n)
+		//{
+		//	cout << "Podaj liczbe punktow studenta: ", cin >> a;
+		//	if (a >= 0 && a <= 100) {
+		//		
+		//		i++;
+		//		suma += a;
+		//	}
+		//	else if (a < 0 || a > 100) {
+		//		continue;
+		//	}
+		//}
+		//cout << suma / n;
+		
+	//-----------Zadanie 3 b)--------------
+	
+	//int n, i, a;
+	//float	suma = 0;
+	//cout << "Podaj liczbe studentow: ", cin >> n;
+	//i = 0;
+	//while (i < n)
+	//{
+	//	cout << "Podaj liczbe punktow studenta: ", cin >> a;
+
+	//		i++;
+	//		suma += a;
+	//	
+	//}
+	//cout << suma / n;
+	
+
+	//-----------Zadanie 4-------------
+	
+	//int n, i, a;
+	//float	suma = 0;
+	//cout << "Podaj liczbe studentow: ", cin >> n;
+	//i = 0;
+	//do {
+	//	cout << "Podaj liczbe punktow studenta: ", cin >> a;
+	//	
+	//	i++;
+	//	suma += a;
+	//	
+	//} while (i < n);
+	//cout << suma / n;
+	
+
+	//-----------------Zadanie 5 a)-------------
+	
+	//int i;
+	//for (int i = 0; i <= 100; i++) {
+	//	cout << i << ", ";
+	//}
+
+	//-----------------Zadanie 5 b)-------------
+
+	//int i;
+	//for (int i = 100; i >= 0; i--) {
+	//	cout << i << ", ";
+	//}
+
+	//-----------------Zadanie 5 c)-------------
+
+	//int i;
+	//for (int i = 7; i <=77; i=i+7) {
+	//	cout << i << ", ";
+	//}
+
+	//-----------------Zadanie 5 d)-------------
+
+	int i;
+	for (int i = 20; i >=0; i = i - 2) {
+		cout << i << ", ";
+	}
+}
+
+
+
